e4c79ab2abd22e6b3be22aea3ec77b47d144e47c
tomazz <tomaz.iskaman@gmail.com>
2015-05-08 20:56:07 +0200

    trigger function (translate) and some more translation examples added

--- translator.js
+++ translator.js
@@ -1,29 +1,37 @@
 $( document ).ready(function() {
     //CHANGE HERE: //////////////////////////////////////////////////
-    var baseLanguage='sl'; //jezik, v katerem so originalni napisi
+    var baseLanguage='en'; //jezik, v katerem so originalni napisi
     var translations=
     [
-     {'sl':'shrani meritev','en':'save measurement'},
-     {'sl':'izvoz','en':'export'}
+        {'sl':'kanal','en':'Channel'},
+        {'sl':'čas [ μs ]','en':'Time [ μs ]'},
+        {'sl':'napetost','en':'Voltage'},
+        {'sl':'Povprečenje','en':'Averaging'},
+        {'sl':'Ponastavi','en':'Reset'}
     ];
     ///////////////////////////////////////////////////
-    function translator(fromLang,toLang)
+    function translator(fromLang,toLang,el)
     {
         if (toLang===undefined || toLang=='') toLang=baseLanguage;
         if (fromLang===undefined || fromLang=='') fromLang=baseLanguage;
         
-            $('body *').contents().filter(function() { 
-                if (this.nodeType == 3)
+        var selection;
+        if (el===undefined)
+            selection=$('body *');
+        else
+            selection=$(el);
+            
+        selection.contents().filter(function() { 
+            if (this.nodeType == 3)
+            {
+                for (var i=0;i<translations.length;++i)
                 {
-                    for (var i=0;i<translations.length;++i)
-                    {
-                        if (this.nodeValue==translations[i][fromLang]) {
-                            this.nodeValue=translations[i][toLang];
-                        }
-                    }
+                    this.nodeValue=this.nodeValue.replace(translations[i][fromLang],translations[i][toLang]);
                 }
-            });
+            }
+        });
     }
     translator(baseLanguage,location.hash.substr(1));
     $('.translate').click(function(){translator(location.hash.substr(1),$(this).attr('href').substr(1))});
+    $('body *').on("translate",function(){translator(baseLanguage,location.hash.substr(1),this)});
 });
